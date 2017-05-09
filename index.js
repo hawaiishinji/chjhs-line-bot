da5014f41a31f9d2648197035b7fb98e4297a8ed
Hawaii Ho <hawaiishinji@gmail.com>
2017-05-09 22:49:33 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: channel Id,
+  channelSecret: channel Secret,
+  channelAccessToken: channel Access Token
+});
+
+
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
