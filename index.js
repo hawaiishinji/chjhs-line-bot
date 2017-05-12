bef9a99ff8cc987e5710d2a394c031e6735fbcc6
Hawaii Ho <hawaiishinji@gmail.com>
2017-05-12 10:56:34 +0800

    remove auto reply

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ bot.on('message', function(event) {
     //console.log(event); //把收到訊息的 event 印出來看看
     if (event.message.type = 'text') {
         var msg = event.message.text;
-        event.reply(msg).then(function(data) {
+        /*    event.reply(msg).then(function(data) {
             // success
             console.log(msg);
         }).catch(function(error) {
             // error
             console.log('error');
-        });
+        });*/
     }
 });
 
