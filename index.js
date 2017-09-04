a7c0ad0a0fbb6b8938355543aa27c70a79afa332
Hawaii Ho <hawaiishinji@gmail.com>
2017-09-05 00:52:15 +0800

    update url path for the ECELE1B class

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ bot.on('message', function(event) {
     //console.log(event); //把收到訊息的 event 印出來看看
     if (event.message.type = 'text') {
         var msg = event.message.text;
+        console.log('message ' + msg);
         /*    event.reply(msg).then(function(data) {
             // success
             console.log(msg);
@@ -57,7 +58,7 @@ bot.on('leave', function(event) {
 
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+app.post('/ECELE1B', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
