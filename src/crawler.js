a690a3b60f46c6d92a40de41e25a914e0f7d5f7b
Hawaii Ho <hawaiishinji@gmail.com>
2017-09-18 23:48:32 +0800

    refactor the crawlTheUrl

--- src/crawler.js
+++ src/crawler.js
@@ -7,127 +7,123 @@ var crawler = new Crawler().configure({ignoreRelative: false, depth: 1});
 var classes = require('./classes');
 
 String.prototype.replaceAll = function (find, replace) {
-    var str = this;
-    return str.replace(new RegExp(find, 'g'), replace);
+  var str = this;
+  return str.replace(new RegExp(find, 'g'), replace);
 };
 
 const crawl = (url) =>{
-    return new Promise((resolve, reject) => {
-        crawler.crawl({
-            url: url,
-            success: (page) => resolve(page),
-            failure: (page) => reject(page)
-        });
+  return new Promise((resolve, reject) => {
+    crawler.crawl({
+      url: url,
+      success: (page) => resolve(page),
+      failure: (page) => reject(page)
     });
+  });
 };
 
 
 const getSelector = (page) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
-        const html = page.content.toString();
-        const selector = Cheerio.load(html);
-        resolve(selector);
-    });
+    const html = page.content.toString();
+    const selector = Cheerio.load(html);
+    resolve(selector);
+  });
 };
 
 const checkHasContent = (selector) => {
-    return new Promise((resolve, reject) =>{
-        const lis = selector('div.ecbookdetail li');
-        if (lis.length > 0){
-            resolve(selector);
-        }
-        else{
-            throw new Error('no content!');
-        }
-    });
+  return new Promise((resolve, reject) =>{
+    const lis = selector('div.ecbookdetail li');
+    if (lis.length > 0){
+      resolve(selector);
+    }
+    else{
+      throw new Error('no content!');
+    }
+  });
 
 };
 
 const getContent = (selector, className) => {
-    return new Promise((resolve, reject) =>{
-        var resultString = '';
+  return new Promise((resolve, reject) =>{
+    var resultString = '';
 
-        //get the day string
-        const mon = selector('div.todayarea .mm');
-        const day = selector('div.todayarea .dd');
-        var dayString = selector(mon[0]).text() + ' ' +  selector(day[0]).text();
-        resultString = className + ' ' + dayString + '\n';
+    //get the day string
+    const mon = selector('div.todayarea .mm');
+    const day = selector('div.todayarea .dd');
+    var dayString = selector(mon[0]).text() + ' ' +  selector(day[0]).text();
+    resultString = className + ' ' + dayString + '\n';
 
-        const result = selector('div.ecbookdetail li,h5');
-        for(let i = 0;i<result.length;i++) {
-            console.log(result[i].name);
-            //insert change line before header
-            if (result[i].name == 'h5'){
-                resultString += '\n';
-            }
-            selector('i').remove();
-            //resultString += selector(result[i]).text().replaceAll('[\t]' ,'') + '\n';
+    const result = selector('div.ecbookdetail li,h5');
+    for(let i = 0;i<result.length;i++) {
+      console.log(result[i].name);
+      //insert change line before header
+      if (result[i].name == 'h5'){
+        resultString += '\n';
+      }
+      selector('i').remove();
+      //resultString += selector(result[i]).text().replaceAll('[\t]' ,'') + '\n';
 
-            //convert <br> to change line
-            resultString += he.decode(selector(result[i]).html()).replaceAll('<br>' ,'\n\n') + '\n';
-            //insert change line after header
-            if (result[i].name == 'h5'){
-                resultString += '\n';
-            }
-        }
-        console.log(resultString);
+      //convert <br> to change line
+      resultString += he.decode(selector(result[i]).html()).replaceAll('<br>' ,'\n\n') + '\n';
+      //insert change line after header
+      if (result[i].name == 'h5'){
+        resultString += '\n';
+      }
+    }
+    console.log(resultString);
 
-        resolve({dayString: dayString, contentString: resultString});
-    });
+    resolve({dayString: dayString, contentString: resultString});
+  });
 };
 
-function crawlTheUrl(url, classId, className, bot){
-    return crawl(url)
-    .then((page) => {
-        return getSelector(page);
-    })
-    .then((selector) =>{
-        return checkHasContent(selector);
-    })
-    .then((selector) => {
-        return getContent(selector, className);
-    })
-    .then((content) => {
+function crawlTheUrl(theClass){
+  const {url, name} = theClass;
+  const classId = theClass.id;
 
-        console.log(content);
+  return crawl(url)
+  .then((page) => {
+    return getSelector(page);
+  })
+  .then((selector) =>{
+    return checkHasContent(selector);
+  })
+  .then((selector) => {
+    return getContent(selector, name);
+  })
+  .then((content) => {
 
+    console.log(content);
 
-            dbTool.findLastestContent(classId).then((contentObject) =>{
+    dbTool.findLastestContent(classId).then((contentObject) =>{
 
-                if ((contentObject) && (contentObject.dayString == content.dayString)){
-                    console.log('this content already exist in db');
-                }
-                else if ((!contentObject) || (contentObject.dayString != content.dayString)){
+      if ((contentObject) && (contentObject.dayString == content.dayString)){
+        console.log('this content already exist in db');
+      }
+      else if ((!contentObject) || (contentObject.dayString != content.dayString)){
 
-                    // clean old content and insert new day
-                    dbTool.cleanContentDb(classId);
+        // clean old content and insert new day
+        dbTool.cleanContentDb(classId);
 
-                    dbTool.insertContent(classId, content.dayString, content.contentString);
+        dbTool.insertContent(classId, content.dayString, content.contentString);
 
-                    // send content to each id
-                    dbTool.findId(classId).then((ids)=>{
-                        console.log('multicast to ' + ids);
-                        bot.push(ids, content.contentString);
-                    });
-
-
-                }
-
-            });
-
-
-    })
-    .catch((err) => console.log(err.message));
+        // send content to each id
+        dbTool.findId(classId).then((ids)=>{
+          console.log('multicast to ' + ids);
+          bot.push(ids, content.contentString);
+        });
+      }
+    });
+  })
+  .catch((err) => console.log(err.message));
 }
 
 var bot = linebot({
-            channelId:  process.env.ChannelId,
-            channelSecret:  process.env.ChannelSecret,
-            channelAccessToken:  process.env.ChannelAccessToken
-        });
+  channelId:  process.env.ChannelId,
+  channelSecret:  process.env.ChannelSecret,
+  channelAccessToken:  process.env.ChannelAccessToken
+});
 
-
-crawlTheUrl(classes[0].url, classes[0].id, classes[0].name, bot).then(() =>{
-  crawlTheUrl(classes[1].url, classes[1].id, classes[1].name, bot);
+crawlTheUrl(classes[0]).then(() =>{
+  crawlTheUrl(classes[1]);
 });
