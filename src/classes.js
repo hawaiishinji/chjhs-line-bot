414d5e67637b4afefa2eb207e2dd40f1dc75d7bf
Hawaii Ho <hawaiishinji@gmail.com>
2018-08-28 22:52:20 +0800

    change description and update the class list

--- src/classes.js
+++ src/classes.js
@@ -30,43 +30,7 @@ module.exports = [
     url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2F',
     name:'二年義班'
   },
-  // 一年級
-  {
-    id: 'ECELE1A',
-    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1A',
-    name:'一年忠班'
-  },
-  {
-    id: 'ECELE1B',
-    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1B',
-    name:'一年孝班'
-  },
-  {
-    id: 'ECELE1C',
-    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1C',
-    name:'一年仁班'
-  },
-  {
-    id: 'ECELE1D',
-    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1D',
-    name:'一年愛班'
-  },
-  {
-    id: 'ECELE1E',
-    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1E',
-    name:'一年信班'
-  },
-  {
-    id: 'ECELE1F',
-    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1F',
-    name:'一年義班'
-  },
   // 幼稚園
-  {
-    id: 'ECKID1C',
-    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID1C',
-    name:'鸚鵡班'
-  },
   {
     id: 'ECKID2B',
     url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID2B',
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ var bot = linebot({
   channelAccessToken:  process.env.ChannelAccessToken
 });
 
-const initialMessage = '目前僅支援鸚鵡班與一年級所有班級\n'
+const initialMessage = '目前僅支援企鵝班,螃蟹班與二年級所有班級\n'
                         +'請問你要關注哪一班\n'
                         +'請用"小幫手我要關注" 或 "小幫手我要退訂" 加上班級名稱來關注或退訂聯絡簿'
                         ;
