d64dd2c8c85a4a2d7e6ef09d6b622ff60b0bdbc3
Hawaii Ho <hawaiishinji@gmail.com>
2018-08-28 20:41:35 +0800

    update to new class

--- src/classes.js
+++ src/classes.js
@@ -1,4 +1,36 @@
 module.exports = [
+  // 二年級
+  {
+    id: 'ECELE2A',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2A',
+    name:'二年忠班'
+  },
+  {
+    id: 'ECELE2B',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2B',
+    name:'二年孝班'
+  },
+  {
+    id: 'ECELE2C',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2C',
+    name:'二年仁班'
+  },
+  {
+    id: 'ECELE2D',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2D',
+    name:'二年愛班'
+  },
+  {
+    id: 'ECELE2E',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2E',
+    name:'二年信班'
+  },
+  {
+    id: 'ECELE2F',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2F',
+    name:'二年義班'
+  },
+  // 一年級
   {
     id: 'ECELE1A',
     url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1A',
@@ -29,10 +61,21 @@ module.exports = [
     url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE1F',
     name:'一年義班'
   },
+  // 幼稚園
   {
     id: 'ECKID1C',
     url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID1C',
     name:'鸚鵡班'
+  },
+  {
+    id: 'ECKID2B',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID2B',
+    name:'螃蟹班'
+  },
+  {
+    id: 'ECKID2C',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID2C',
+    name:'企鵝班'
   },
 
 ];
