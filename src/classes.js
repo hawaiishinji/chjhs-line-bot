6f0a20a79f189434fb71ce991281cd563ca28cd3
Hawaii Ho <hawaiishinji@gmail.com>
2019-08-29 11:27:35 +0800

    update classes to next year

--- src/classes.js
+++ src/classes.js
@@ -30,6 +30,37 @@ module.exports = [
     url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE2F',
     name:'二年義班'
   },
+   // 三年級
+   {
+    id: 'ECELE3A',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE3A',
+    name:'三年忠班'
+  },
+  {
+    id: 'ECELE3B',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE3B',
+    name:'三年孝班'
+  },
+  {
+    id: 'ECELE3C',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE3C',
+    name:'三年仁班'
+  },
+  {
+    id: 'ECELE3D',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE3D',
+    name:'三年愛班'
+  },
+  {
+    id: 'ECELE3E',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE3E',
+    name:'三年信班'
+  },
+  {
+    id: 'ECELE3F',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/ELEFULL.aspx?ddsPageID=ELEECONTACT&&classid=ECELE3F',
+    name:'三年義班'
+  },
   // 幼稚園
   {
     id: 'ECKID2A',
@@ -45,6 +76,22 @@ module.exports = [
     id: 'ECKID2C',
     url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID2C',
     name:'企鵝班'
+  },
+
+  {
+    id: 'ECKID3A',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID3A',
+    name:'大象班'
+  },
+  {
+    id: 'ECKID3B',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID3B',
+    name:'綿羊班'
+  },
+  {
+    id: 'ECKID3C',
+    url: 'http://www.chjhs.tp.edu.tw/dispPageBox/KIDFULL.aspx?ddsPageID=KIDECONTACT&&classid=ECKID3C',
+    name:'長頸鹿班'
   },
 
 ];
